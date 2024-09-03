e44155abbf985e71ca13af73f5705f55987ad284
nosorog182013-zys <nosorog182013@gmail.com>
2024-09-03 22:17:07 +0800

    Add files via upload

---
+++ lesson603.py
@@ -0,0 +1,45 @@
+class Horse:
+  def __init__(self, x_distance, sound):
+    self.x_distance = 0
+    self.sound = 'Frrr'
+
+  def run(self, dx):
+    self.x_distance += dx
+    return self.x_distance
+
+class Eagle:
+  def __init__(self, y_distance, sound):
+    self.y_distance = 0
+    self.sound = 'I train, eat, sleep, and repeat'
+
+  def fly(self, dy):
+    self.y_distance += dy
+    return self.y_distance
+
+class Pegasus(Eagle, Horse):
+  def __init__(self, x_distance=0, y_distance=0):
+    super().__init__(x_distance, y_distance)
+    self.x_distance = x_distance
+    self.y_distance = y_distance
+    
+  def move(self, dx, dy):
+    Horse.run(self, dx)
+    Eagle.fly(self, dy)
+    return self.x_distance, self.y_distance
+
+  def get_pos(self):
+    return (self.x_distance, self.y_distance)
+
+  def voice(self):
+    print(f"{self.sound}")
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
+
