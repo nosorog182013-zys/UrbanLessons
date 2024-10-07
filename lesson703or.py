f979522187ee8f8b5f51994bcaa4427f0f858c52
nosorog182013-zys <nosorog182013@gmail.com>
2024-10-08 00:54:22 +0800

    Update lesson703or.py

--- lesson703or.py
+++ lesson703or.py
@@ -1,16 +1,10 @@
+#Домашнее задание по теме "Позиционирование в файле".
 def custom_write(file_name, strings):
-  """
-  Записывает в файл file_name все строки из списка strings, каждая на новой строке.
-  Возвращает словарь strings_positions, где ключом будет кортеж 
-  (<номер строки>, <байт начала строки>), а значением - записываемая строка.
-  """
   strings_positions = {}
   with open(file_name, "w") as f:
     for i, string in enumerate(strings):
       f.write(string + "\n")
-      # Получаем текущую позицию файла
       current_position = f.tell()
-      # Находим позицию начала текущей строки, отнимая длину текущей строки и символа новой строки
       start_position = current_position - len(string) - 1
       strings_positions[(i + 1, start_position)] = string
   return strings_positions
@@ -26,4 +20,3 @@ strings = [
 result = custom_write('test.txt', strings)
 for elem in result.items():
   print(elem)
-
