8088f060c1c6845b684de2a8fa9ceb931946a3e8
nosorog182013-zys <nosorog182013@gmail.com>
2024-11-22 16:22:16 +0800

    Update lesson801.py

--- lesson801.py
+++ lesson801.py
@@ -1,6 +1,7 @@
+#2023/11/24 00:00|Домашнее задание по теме "Try и Except"
 def add_everything_up(a, b):
   try:
-    return a + b
+    return round(a + b, 3)
   except TypeError:
     print(f"{a}{b}")
 
