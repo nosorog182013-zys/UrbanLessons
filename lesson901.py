0c730a547c607373cba5b931f74032a50f367113
nosorog182013-zys <nosorog182013@gmail.com>
2024-12-05 14:03:42 +0800

    Add files via upload

---
+++ lesson901.py
@@ -0,0 +1,10 @@
+def apply_all_func(int_list, *functions):
+  results = {}
+  for function in functions:
+    function_name = function.__name__
+    results[function_name] = function(int_list)
+  return results
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
