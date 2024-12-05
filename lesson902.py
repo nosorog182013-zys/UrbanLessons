588bd618febba02f553e7f55092ae636f9141aae
nosorog182013-zys <nosorog182013@gmail.com>
2024-12-05 20:26:25 +0800

    Add files via upload

---
+++ lesson902.py
@@ -0,0 +1,13 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+all_strings = first_strings + second_strings
+
+first_result = [len(first_string) for first_string in first_strings if len(first_string) > 5]
+
+second_result = [(first_string, second_string) for first_string in first_strings for second_string in second_strings if len(first_string) == len(second_string)]
+
+third_result = {all_string: len(all_string) for all_string in all_strings if len(all_string) // 2}
+
+print(first_result)
+print(second_result)
+print(third_result)
