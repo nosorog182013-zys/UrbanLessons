e0c518025e9d4af8e69c56b2cd5e4f7fad934f58
nosorog182013-zys <nosorog182013@gmail.com>
2024-12-07 21:02:13 +0800

    Add files via upload

---
+++ lesson903.py
@@ -0,0 +1,7 @@
+#https://itresume.ru/blog/python-7-zip-levels
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+first_result = [(len(a) - len(b)) for a, b in zip(first, second) if len(a) != len(b)] 
+second_result = [(len(first[i]) == len(second[i])) for i in range(len(first))]
+print(list(first_result))
+print(list(second_result))
